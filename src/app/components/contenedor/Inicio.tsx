57029ce882c75c424f1f3bc217bb8c01fbef2faa
ccastibl <ccastibl@cuc.edu.co>
2024-10-28 09:52:36 -0500

    m2

--- src/app/components/contenedor/Inicio.tsx
+++ src/app/components/contenedor/Inicio.tsx
@@ -1,9 +1,21 @@
 export const Inicio = () => {
-    return (
-      <>
-        Hola soy el Inicio <br />
-        Como el binvenido
-      </>
-    );
-  };
-  
+  return (
+    <>
+    <div className="pt-4 d-flex justify-content-center">
+      <div className="col-md-6">
+        <div className="h-100 p-5 text-bg-dark rounded-3">
+          <h2>Bienvenidos</h2>
+          <p>
+            <b>
+              Bienvenidos a Peliculas Star, no somos como royal films o cinecolombia, nosotros somos los mejores
+            </b>
+          </p>
+          <button className="btn btn-outline-light" type="button">
+            Example button
+          </button>
+        </div>
+      </div>
+    </div>
+    </>
+  );
+};
--- src/app/utilidades/rutas/Ruteo.tsx
+++ src/app/utilidades/rutas/Ruteo.tsx
@@ -1,10 +1,22 @@
-import { Route,Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import { Inicio } from "../../components/contenedor/Inicio";
+import { PeliCrear } from "../../components/peliculas/PeliCrear";
+import { PeliListar } from "../../components/peliculas/PeliListar";
+import { PeliAdmin } from "../../components/peliculas/PeliAdmin";
+import { PeliActualizar } from "../../components/peliculas/PeliActualizar";
+import { Acerca } from "../../components/otros/Acerca";
+import { NoEncontrado } from "../../components/contenedor/NoEncontrado";
 
-export const Ruteo= ()=>{
-    return(
-        <Routes>
-            <Route path="/" element={<Inicio/>}/>
-        </Routes>
-    )
-}
+export const Ruteo = () => {
+  return (
+    <Routes>
+      <Route path="/pelicre" element={<PeliCrear />} />
+      <Route path="/pelilis" element={<PeliListar />} />
+      <Route path="/peliadm" element={<PeliAdmin />} />
+      <Route path="/peliact/:codigo" element={<PeliActualizar />} />
+      <Route path="/acer" element={<Acerca />} />
+      <Route path="*" element={<NoEncontrado />} />
+      <Route path="/" element={<Inicio />} />
+    </Routes>
+  );
+};
